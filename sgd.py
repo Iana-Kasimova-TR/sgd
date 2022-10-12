9f56f981908279717c1e3c8d5c51ff43b193da6d
Iana Kasimova <Iana_Kasimova@epam.com>
2022-10-12 17:00:24 +0200

    commit

---
+++ sgd.py
@@ -0,0 +1,199 @@
+from nltk import word_tokenize
+import csv
+import pandas as pd
+import numpy as np
+from nltk.corpus import stopwords
+from collections import Counter
+from scipy.sparse import csr_matrix
+import re
+stop_words = stopwords.words('english')
+
+
+def read_source(path_to_file):
+    number_title = 1
+    number_text = 2
+    token_number: dict = {}
+    dict_title: dict =  {}
+    dict_text: dict = {}
+    dict_incorrect_text: dict = {}
+    counter = 0
+    with open(path_to_file, newline="", encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        next(reader)
+        for line in reader:
+            title = clean_text(line[number_title]).split()
+            text = clean_text(line[number_text]).split()
+            line = title + text
+            for word_item in line:
+                token_number.setdefault(word_item, len(token_number)) 
+            dict_title[counter] = [token_number.get(item) for item in title] 
+            dict_text[counter] = [token_number.get(item) for item in text]
+            if (counter > 0):
+                dict_incorrect_text[counter] = dict_text.get(counter - 1)
+            counter += 1    
+
+    dict_incorrect_text[0] = dict_text.get(counter-1)
+
+    return token_number, dict_title, dict_text, dict_incorrect_text
+
+    
+#Removes Punctuations
+def remove_punctuations(data):
+    punct_tag=re.compile(r'[^\w\s]')
+    data=punct_tag.sub(r'',data)
+    return data
+
+#Removes HTML syntaxes
+def remove_html(data):
+    html_tag=re.compile(r'<.*?>')
+    data=html_tag.sub(r'',data)
+    return data
+
+#Removes URL data
+def remove_url(data):
+    url_clean= re.compile(r"https://\S+|www\.\S+")
+    data=url_clean.sub(r'',data)
+    return data
+
+#Removes Emojis
+def remove_emoji(data):
+    emoji_clean= re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    data=emoji_clean.sub(r'',data)
+    url_clean= re.compile(r"https://\S+|www\.\S+")
+    data=url_clean.sub(r'',data)
+    return data
+
+    
+def remove_abb(data):
+    data = re.sub(r"he's", "he is", data)
+    data = re.sub(r"there's", "there is", data)
+    data = re.sub(r"We're", "We are", data)
+    data = re.sub(r"That's", "That is", data)
+    data = re.sub(r"won't", "will not", data)
+    data = re.sub(r"they're", "they are", data)
+    data = re.sub(r"Can't", "Cannot", data)
+    data = re.sub(r"wasn't", "was not", data)
+    data = re.sub(r"don\x89Ûªt", "do not", data)
+    data= re.sub(r"aren't", "are not", data)
+    data = re.sub(r"isn't", "is not", data)
+    data = re.sub(r"What's", "What is", data)
+    data = re.sub(r"haven't", "have not", data)
+    data = re.sub(r"hasn't", "has not", data)
+    data = re.sub(r"There's", "There is", data)
+    data = re.sub(r"He's", "He is", data)
+    data = re.sub(r"It's", "It is", data)
+    data = re.sub(r"You're", "You are", data)
+    data = re.sub(r"I'M", "I am", data)
+    data = re.sub(r"shouldn't", "should not", data)
+    data = re.sub(r"wouldn't", "would not", data)
+    data = re.sub(r"i'm", "I am", data)
+    data = re.sub(r"I\x89Ûªm", "I am", data)
+    data = re.sub(r"I'm", "I am", data)
+    data = re.sub(r"Isn't", "is not", data)
+    data = re.sub(r"Here's", "Here is", data)
+    data = re.sub(r"you've", "you have", data)
+    data = re.sub(r"you\x89Ûªve", "you have", data)
+    data = re.sub(r"we're", "we are", data)
+    data = re.sub(r"what's", "what is", data)
+    data = re.sub(r"couldn't", "could not", data)
+    data = re.sub(r"we've", "we have", data)
+    data = re.sub(r"it\x89Ûªs", "it is", data)
+    data = re.sub(r"doesn\x89Ûªt", "does not", data)
+    data = re.sub(r"It\x89Ûªs", "It is", data)
+    data = re.sub(r"Here\x89Ûªs", "Here is", data)
+    data = re.sub(r"who's", "who is", data)
+    data = re.sub(r"I\x89Ûªve", "I have", data)
+    data = re.sub(r"y'all", "you all", data)
+    data = re.sub(r"can\x89Ûªt", "cannot", data)
+    data = re.sub(r"would've", "would have", data)
+    data = re.sub(r"it'll", "it will", data)
+    data = re.sub(r"we'll", "we will", data)
+    data = re.sub(r"wouldn\x89Ûªt", "would not", data)
+    data = re.sub(r"We've", "We have", data)
+    data = re.sub(r"he'll", "he will", data)
+    data = re.sub(r"Y'all", "You all", data)
+    data = re.sub(r"Weren't", "Were not", data)
+    data = re.sub(r"Didn't", "Did not", data)
+    data = re.sub(r"they'll", "they will", data)
+    data = re.sub(r"they'd", "they would", data)
+    data = re.sub(r"DON'T", "DO NOT", data)
+    data = re.sub(r"That\x89Ûªs", "That is", data)
+    data = re.sub(r"they've", "they have", data)
+    data = re.sub(r"i'd", "I would", data)
+    data = re.sub(r"should've", "should have", data)
+    data = re.sub(r"You\x89Ûªre", "You are", data)
+    data = re.sub(r"where's", "where is", data)
+    data = re.sub(r"Don\x89Ûªt", "Do not", data)
+    data = re.sub(r"we'd", "we would", data)
+    data = re.sub(r"i'll", "I will", data)
+    data = re.sub(r"weren't", "were not", data)
+    data = re.sub(r"They're", "They are", data)
+    data = re.sub(r"Can\x89Ûªt", "Cannot", data)
+    data = re.sub(r"you\x89Ûªll", "you will", data)
+    data = re.sub(r"I\x89Ûªd", "I would", data)
+    data = re.sub(r"let's", "let us", data)
+    data = re.sub(r"it's", "it is", data)
+    data = re.sub(r"can't", "cannot", data)
+    data = re.sub(r"don't", "do not", data)
+    data = re.sub(r"you're", "you are", data)
+    data = re.sub(r"i've", "I have", data)
+    data = re.sub(r"that's", "that is", data)
+    data = re.sub(r"i'll", "I will", data)
+    data = re.sub(r"doesn't", "does not",data)
+    data = re.sub(r"i'd", "I would", data)
+    data = re.sub(r"didn't", "did not", data)
+    data = re.sub(r"ain't", "am not", data)
+    data = re.sub(r"you'll", "you will", data)
+    data = re.sub(r"I've", "I have", data)
+    data = re.sub(r"Don't", "do not", data)
+    data = re.sub(r"I'll", "I will", data)
+    data = re.sub(r"I'd", "I would", data)
+    data = re.sub(r"Let's", "Let us", data)
+    data = re.sub(r"you'd", "You would", data)
+    data = re.sub(r"It's", "It is", data)
+    data = re.sub(r"Ain't", "am not", data)
+    data = re.sub(r"Haven't", "Have not", data)
+    data = re.sub(r"Could've", "Could have", data)
+    data = re.sub(r"youve", "you have", data)  
+    data = re.sub(r"donå«t", "do not", data)
+    return data  
+
+    
+def clean_text(text):
+    text = remove_abb(text)
+    text = remove_abb(text)
+    text = remove_punctuations(text)
+    text = remove_punctuations(text)
+    text = remove_emoji(text)
+    text = remove_emoji(text)
+    text = remove_html(text)
+    text = remove_html(text)
+    text = remove_url(text)
+    text = remove_url(text)
+    return text
+
+def calculate_loss(anchor, truth, wrong):
+    return max(0, - np.dot(anchor, truth) + np.dot(anchor, wrong))  
+
+
+def calculate_gradient(anchor, truth, wrong):
+    return (-truth + wrong, -anchor, anchor)
+
+
+def doc_to_vec(doc_indexes, mtx_embed):
+    return np.mean(mtx_embed[doc_indexes], axis = 0)
+
+def get_document_term_sparse_mtx(doc_indices, words_dictionary):
+    number_of_docs = len(doc_indices)
+    row_indices = [np.full(len(item), idx) for idx, item in enumerate(doc_indices.values())]
+    doc_indices = [item for sublist in doc_indices.values() for item in sublist]
+    row_indices = [item for sublist in row_indices for item in sublist]
+    values = np.ones(len(doc_indices))
+    return csr_matrix((values, (row_indices, doc_indices)), shape=(number_of_docs, len(words_dictionary)))
